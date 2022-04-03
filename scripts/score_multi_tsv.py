2be74ff914b9afb44a14313e4b7b75c05c6c02c5
Kalpesh Krishna <kalpeshk2011@gmail.com>
2022-04-03 13:36:46 -0400

    Update scoring script

--- scripts/score_multi_tsv.py
+++ scripts/score_multi_tsv.py
@@ -1,11 +1,13 @@
 import argparse
 import json
 import random
+import random
 import numpy as np
 import mauve
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', default="data/multi_outs/t5_xxl_descartes_wiki_greedy.tsv")
+parser.add_argument('--num_samples', default=1, type=int)
 args = parser.parse_args()
 
 with open(args.dataset, 'r') as f:
@@ -13,10 +15,17 @@ with open(args.dataset, 'r') as f:
 
 all_human = []
 all_gen = []
-for i in range(0, len(data), 2):
-    assert data[i][0] == data[i + 1][0]
+assert len(data) % (args.num_samples + 1) == 0
+
+for i in range(0, len(data), args.num_samples + 1):
+    gen_suffices = []
+    for j in range(1, args.num_samples + 1):
+        assert data[i][1] != data[i + j][1]
+        assert data[i][0] == data[i + j][0]
+        gen_suffices.append(data[i + j][1])
+
     all_human.append(data[i][0] + ' ' + data[i][1])
-    all_gen.append(data[i + 1][0] + ' ' + data[i + 1][1])
+    all_gen.append(data[i][0] + ' ' + random.choice(gen_suffices))
 
 mauve1 = mauve.compute_mauve(p_text=all_gen, q_text=all_human, device_id=0, max_text_length=768, verbose=False)
 print(mauve1.mauve)
