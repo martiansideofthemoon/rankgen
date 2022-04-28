2cd579acf39f201ab2b3beed41625848c0a3d3a7
lilakk <ellachang1002@outlook.com>
2022-04-27 20:49:24 -0400

    consider sentence boundaries

--- scripts/rankgen_beam_search_shifting.py
+++ scripts/rankgen_beam_search_shifting.py
@@ -8,12 +8,16 @@ import os
 import torch
 import random
 import json
+import nltk
+from nltk.tokenize import sent_tokenize
 from functools import partial
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from utils import form_partitions
 from t5x_embeddings import T5XEmbeddingGenerator
 from utils import truncate
 from transformers.utils import logging
+
+nltk.download('punkt')
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
@@ -63,26 +67,37 @@ def postprocess(outputs):
     return tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
 
-def scorer_t5x(t5x_embedder, prefix, suffixes, prefix_vector=None):
-    if prefix_vector is None:
-        prefix_vector = t5x_embedder.encode(prefix, vectors_type="prefix")["embeddings"]
+def truncate(text):
+    last_punc = 0
+    if "." in text:
+        last_punc = max(last_punc, text.rindex("."))
+    if "?" in text:
+        last_punc = max(last_punc, text.rindex("?"))
+    if "!" in text:
+        last_punc = max(last_punc, text.rindex("!"))
+    if last_punc != 0:
+        text = text[:last_punc + 1]
+    return text
+
+
+def scorer_t5x(t5x_embedder, prefixes, suffixes, prefix_vectors=None):
+    if prefix_vectors is None:
+        prefix_vectors = t5x_embedder.encode(prefixes, vectors_type="prefix")["embeddings"]
     suffix_vectors = t5x_embedder.encode(suffixes, vectors_type="suffix")["embeddings"]
-    similarities = torch.matmul(prefix_vector, suffix_vectors.t()).squeeze(dim=0)
-    return similarities, prefix_vector, suffix_vectors
+    matmul = torch.matmul(prefix_vectors, suffix_vectors.t()).squeeze(dim=0)
+    similarities = torch.from_numpy(np.diagonal(matmul.cpu().numpy()))
+    return similarities, prefix_vectors, suffix_vectors
 
 
-def token_beam_search(contexts, scorer, beam_size=3, temperature=1.0, top_p=0.9, num_tokens=5, num_samples=10, max_length=115):
+def token_beam_search(contexts, scorer, beam_size=3, temperature=1.0, top_p=0.9, num_tokens=5, num_samples=10,
+                      max_length=115):
     final_outputs = []
     final_scores = []
     total_generated_tokens = 0
     for ctx in contexts:
         ctx_tokens = tokenizer(ctx, truncation=True, padding="longest", return_tensors="pt")['input_ids']
-        num_ctx_tokens = ctx_tokens.size()[1]
-        if beam_size == 1 and num_samples == 1:
-            prefix_vector = None
-        else:
-            _, prefix_vector, _ = scorer(prefix=ctx, suffixes=[ctx])
         beams = [{
+            "prefix_text": ctx,
             "prefix_tokens": ctx_tokens,
             "text": "",
             "eos": False
@@ -110,20 +125,37 @@ def token_beam_search(contexts, scorer, beam_size=3, temperature=1.0, top_p=0.9,
                         is_eos.append(False)
                 curr_outs_text = postprocess(curr_outs['sequences'][:, num_input_tokens:])
                 for tokens, text, eos in zip(curr_outs['sequences'][:, num_input_tokens:], curr_outs_text, is_eos):
-                    # update all_outs
-                    prefix_tokens = torch.squeeze(beam['prefix_tokens'])
-                    prefix_tokens = prefix_tokens.to(device)
-                    new_prefix_tokens = torch.unsqueeze(torch.cat([prefix_tokens, tokens], dim=0)[-num_ctx_tokens:], dim=0)
-                    all_outs.append({
-                        "prefix_tokens": new_prefix_tokens,
-                        "text": beam["text"] + text,
-                        "eos": eos
-                    })
+                    curr_outs_sents = sent_tokenize(text)
+                    # if a full sentence has been generated
+                    if len(curr_outs_sents) > 1:
+                        # remove first sentence from prefix and append generated sentence to prefix
+                        prefix_sents = nltk.sent_tokenize(beam["prefix_text"])[1:]
+                        prefix_sents.append(curr_outs_sents[0])
+                        while len(tokenizer(' '.join(prefix_sents))['input_ids']) > 256:
+                            prefix_sents.pop(0)
+                        prefix_text = ' '.join(prefix_sents)
+                        prefix_tokens = tokenizer(prefix_text, truncation=True, padding="longest", return_tensors="pt")[
+                            'input_ids']
+                        prefix_tokens = prefix_tokens.to(device)
+                        all_outs.append({
+                            "prefix_text": prefix_text,
+                            "prefix_tokens": prefix_tokens,
+                            "text": beam["text"] + text,
+                            "eos": eos
+                        })
+                    else:
+                        all_outs.append({
+                            "prefix_text": beam['prefix_text'],
+                            "prefix_tokens": beam['prefix_tokens'],
+                            "text": beam["text"] + text,
+                            "eos": eos
+                        })
+
             # Each beam has total_generated_tokens length
             total_generated_tokens += max_new_tokens
             if len(all_outs) > 1:
                 # skip beam scoring if only one output to choose from
-                scores, _, _ = scorer(prefix=ctx, suffixes=[x["text"] for x in all_outs], prefix_vector=prefix_vector)
+                scores, _, _ = scorer(prefixes=[x["prefix_text"] for x in all_outs], suffixes=[x["text"] for x in all_outs], prefix_vectors=None)
                 top_scores, top_indices = torch.topk(scores, k=beam_size)
                 beams = [all_outs[x] for x in top_indices]  # only track the top k beams
             else:
@@ -141,6 +173,7 @@ def token_beam_search(contexts, scorer, beam_size=3, temperature=1.0, top_p=0.9,
                 break
     return final_outputs, final_scores
 
+
 scorer_fn = partial(scorer_t5x, t5x_embedder=t5x_embedder)
 
 outputs = []
@@ -157,8 +190,10 @@ if os.path.exists(args.output_file):
 for kk, instance in tqdm.tqdm(enumerate(data), total=len(data)):
     if kk < len(outputs):
         continue
-    token_beam_text, token_beam_scores = token_beam_search(contexts=[instance["prefix"]], scorer=scorer_fn, beam_size=args.beam_size,
-                                                           top_p=args.top_p, num_tokens=args.num_tokens, num_samples=args.num_samples)
+    token_beam_text, token_beam_scores = token_beam_search(contexts=[instance["prefix"]], scorer=scorer_fn,
+                                                           beam_size=args.beam_size,
+                                                           top_p=args.top_p, num_tokens=args.num_tokens,
+                                                           num_samples=args.num_samples)
 
     token_beam_text = token_beam_text[0]
     token_beam_text = [truncate(" ".join(x.split())) for x in token_beam_text]
