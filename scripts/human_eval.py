d064cd67aaf0a64ab97eb37a9ee703c64533f3e1
lilakk <ellachang1002@outlook.com>
2022-05-01 13:02:53 -0400

    updated human eval script

--- scripts/human_eval.py
+++ scripts/human_eval.py
@@ -6,43 +6,40 @@ import random
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', default="data/t5_xl_all_domains_wiki_hard.jsonl")
-parser.add_argument('--folder', default="ab_tests/gold_neg_wiki_hard")
-parser.add_argument('--method', default="nucleus")
+parser.add_argument('--nucleus_dataset', default="/mnt/nfs/work1/miyyer/kalpesh/projects/presuf-retrieval/files_human_eval/pg19_gpt2_medium.jsonl")
+parser.add_argument('--beam_dataset', default="/mnt/nfs/work1/miyyer/kalpesh/projects/presuf-retrieval/files_human_eval/pg19_t5_xl_beam_2_tokens_20_samples_10.jsonl")
+parser.add_argument('--folder', default="ab_tests/pg19_nucleus_vs_beam")
+parser.add_argument('--dataset', default="pg19")
 parser.add_argument('--num_instances', default=200)
 args = parser.parse_args()
 
 os.makedirs(args.folder, exist_ok=True)
 
-with open(args.dataset, "r") as f:
-    data = [json.loads(x) for x in f.read().strip().split("\n")]
+with open(args.nucleus_dataset, "r") as f:
+    nucleus_data = [json.loads(x) for x in f.read().strip().split("\n")]
+with open(args.beam_dataset, "r") as f:
+    beam_data = [json.loads(x) for x in f.read().strip().split("\n")]
 
 random.seed(43)
 
-output = [["Prefix", "First", "Second", "Order", "InstanceNum", "Folder"]]
+output = [["Prefix", "First", "Second", "Order", "NucleusInstanceNum", "BeamInstanceNum", "Dataset", "Folder", "NucleusPath", "BeamPath"]]
 
-for i, dd in enumerate(data[:args.num_instances]):
-    negative = random.choice(dd["negatives"])
+for i, n in enumerate(nucleus_data[:args.num_instances]):
+    nucleus_gen = random.choice(n['targets'][1:])
     order = random.random()
-    print(dd.keys())
-    if args.method == "nucleus":
-        if order < 0.5:
-            output.append([
-                dd["prefix"], dd["suffix"], negative, "beam,nucleus", i, args.folder
-            ])
-        else:
-            output.append([
-                dd["prefix"], negative, dd["suffix"], "nucleus,beam", i, args.folder
-            ])
-    elif args.method == "beam":
-        if order < 0.5:
-            output.append([
-                dd["prefix"], dd["suffix"], negative, "beam,nucleus", i, args.folder
-            ])
-        else:
-            output.append([
-                dd["prefix"], negative, dd["suffix"], "nucleus,beam", i, args.folder
-            ])
+    beam_instance_num = i
+    for j, b in enumerate(beam_data):
+        if b['prefix'] == n['prefix']:
+            beam_gen = b['targets'][1]
+        beam_instance_num = j
+    if order < 0.5:
+        output.append([
+            n["prefix"], beam_gen, nucleus_gen, "beam,nucleus", i, beam_instance_num, args.dataset, args.folder, args.nucleus_dataset, args.beam_dataset
+        ])
+    else:
+        output.append([
+            n["prefix"], nucleus_gen, beam_gen, "nucleus,beam", i, beam_instance_num, args.dataset, args.folder, args.nucleus_dataset, args.beam_dataset
+        ])
 
 with open(args.folder + "/input.csv", 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
